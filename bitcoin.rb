5ee0b2043f6ba6d3f21539c6edab03fb91296307
wendreo.teixeira <wendreo.teixeira@konduto.com>
2021-04-10 14:27:30 -0300

    Buscando s da api com parametros de terminal

--- bitcoin.rb
+++ bitcoin.rb
@@ -1,2 +1,18 @@
 require 'rest-client'
 require 'json'
+
+url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
+
+days_to_show = ARGV[0] ? ARGV[0].split("=")[1].to_i : 7 
+
+end_date = Date.today.strftime("%Y-%m-%d")
+start_date = (Date.today - days_to_show).strftime("%Y-%m-%d")
+
+params = "?start=#{start_date}&end=#{end_date}"
+
+response = RestClient.get "#{url}#{params}", {
+    content_type: :json,
+    accept: :json
+}
+
+puts response.body
