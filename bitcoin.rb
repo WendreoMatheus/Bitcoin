00e17d670c62a2da76bdd80ee2e486858bd1deb4
wendreo.teixeira <wendreo.teixeira@konduto.com>
2021-04-10 14:38:53 -0300

    Adicionado terminal

--- bitcoin.rb
+++ bitcoin.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 require 'json'
+require 'terminal-table'
 
 url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
 
@@ -15,4 +16,18 @@ response = RestClient.get "#{url}#{params}", {
     accept: :json
 }
 
-puts response.body
+bpi = JSON.parse(response.body)["bpi"]
+
+bpi_keys = bpi.keys
+
+table_data = bpi.map.with_index do |(data, value), i|
+    [
+        Date.parse(data).strftime("%d/%m/%y"),
+        "$#{value.to_f}",
+        (i > 0 ? (bpi[bpi_keys[i]] > bpi[bpi_keys[i - 1]] ? "🡅" : "🡇") : "")
+    ]
+end
+
+table = Terminal::Table.new :headings => ['Data', 'Valor do Bitcoin', '₿'], :rows => table_data
+
+puts table
