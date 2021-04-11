8b5fec0a71d5007c5175ebdedf62bf1db93d0770
wendreo.teixeira <wendreo.teixeira@konduto.com>
2021-04-11 13:11:32 -0300

    Adicionado modulo de api e coin

--- bitcoin.rb
+++ bitcoin.rb
@@ -1,70 +1,48 @@
 # frozen_string_literal: true
 
-require 'rest-client'
-require 'json'
 require 'terminal-table'
 require 'colorize'
 
-def coin_status(bpi, keys, index)
-  return '' if index.zero?
+require_relative 'coin'
+require_relative 'api'
 
-  bpi[keys[index]] > bpi[keys[index - 1]] ? '🡅'.green : '🡇'.red
+# Bitcoin class
+class Bitcoin
+  extend Coin
+  extend Api
+
+  headings = ['Data', 'Valor do Bitcoin', 'Valorização', 'Variação', '₿']
+  center_columns = [0, 4]
+  right_columns = [1, 2, 3]
+
+  days_to_show = ARGV[0] ? ARGV[0].split('=')[1].to_i : 7
+
+  end_date = Date.today.strftime('%Y-%m-%d')
+  start_date = (Date.today - days_to_show).strftime('%Y-%m-%d')
+
+  bpi = get_cotation(start_date, end_date)['bpi']
+
+  bpi_keys = bpi.keys
+
+  table_data = bpi.map.with_index do |(data, value), i|
+    [
+      Date.parse(data).strftime('%d/%m/%y'),
+      "$#{value.to_f}",
+      coin_valorization(bpi, bpi_keys, i),
+      coin_variation(bpi, bpi_keys, i),
+      coin_status(bpi, bpi_keys, i)
+    ]
+  end
+
+  # Configuracoes da tabela
+  table = Terminal::Table.new
+  table.title = "Cotação Bitcoins dos ultimos #{days_to_show} dias"
+  table.headings = headings
+  table.rows = table_data
+
+  # Align columns to center and left
+  center_columns.each { |x| table.align_column(x, :center) }
+  right_columns.each { |x| table.align_column(x, :right) }
+
+  puts table
 end
-
-def coin_variation(bpi, keys, index)
-  return '$0.00' if index.zero?
-
-  total = bpi[keys[index]] - bpi[keys[index - 1]]
-  "$#{total.round(2)}"
-end
-
-def coin_valorization(bpi, keys, index)
-  return '0%' if index.zero?
-
-  total = ((bpi[keys[index]] - bpi[keys[index - 1]]) * 100) / bpi[keys[index]]
-  "#{total.round(2)}%"
-end
-
-url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
-
-headings = ['Data', 'Valor do Bitcoin', 'Valorização', 'Variação', '₿']
-center_columns = [0, 4]
-right_columns = [1, 2, 3]
-
-days_to_show = ARGV[0] ? ARGV[0].split('=')[1].to_i : 7
-
-end_date = Date.today.strftime('%Y-%m-%d')
-start_date = (Date.today - days_to_show).strftime('%Y-%m-%d')
-
-params = "?start=#{start_date}&end=#{end_date}"
-
-response = RestClient.get "#{url}#{params}", {
-  content_type: :json,
-  accept: :json
-}
-
-bpi = JSON.parse(response.body)['bpi']
-
-bpi_keys = bpi.keys
-
-table_data = bpi.map.with_index do |(data, value), i|
-  [
-    Date.parse(data).strftime('%d/%m/%y'),
-    "$#{value.to_f}",
-    coin_valorization(bpi, bpi_keys, i),
-    coin_variation(bpi, bpi_keys, i),
-    coin_status(bpi, bpi_keys, i)
-  ]
-end
-
-# Configuracoes da tabela
-table = Terminal::Table.new
-table.title = "Cotação Bitcoins dos ultimos #{days_to_show} dias"
-table.headings = headings
-table.rows = table_data
-
-# Align columns to center and left
-center_columns.each { |x| table.align_column(x, :center) }
-right_columns.each { |x| table.align_column(x, :right) }
-
-puts table
