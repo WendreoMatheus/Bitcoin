382f59d97258efef6181f59d70b7a0a5fef04612
wendreo.teixeira <wendreo.teixeira@konduto.com>
2021-04-11 13:07:40 -0300

    Criado modulo para fazer os calculos da moeda

---
+++ coin.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Modulo das moedas
+module Coin
+  def coin_status(bpi, keys, index)
+    return '' if index.zero?
+
+    bpi[keys[index]] > bpi[keys[index - 1]] ? '🡅'.green : '🡇'.red
+  end
+
+  def coin_variation(bpi, keys, index)
+    return '$0.00' if index.zero?
+
+    total = bpi[keys[index]] - bpi[keys[index - 1]]
+    "$#{total.round(2)}"
+  end
+
+  def coin_valorization(bpi, keys, index)
+    return '0%' if index.zero?
+
+    total = ((bpi[keys[index]] - bpi[keys[index - 1]]) * 100) / bpi[keys[index]]
+    "#{total.round(2)}%"
+  end
+end
